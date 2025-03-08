18083dfe32ce02b99534bd71672514d05c0542f6
M. Awais Raza <ar1428391@gmail.com>
2025-03-08 15:21:11 +0500

    sons

--- VirtuComm-Fyp-SS1/backend/tts_sub/views.py
+++ VirtuComm-Fyp-SS1/backend/tts_sub/views.py
@@ -5,13 +5,13 @@ from .text_to_audio import process_conversation_pipeline # Import audio generati
 from pydub import AudioSegment
 from .text_to_audio_single import generate_audio_from_plain_text
 from .audio_to_json_single import audio_to_sub_single
-from .utilities import generate_lipsync_for_patch,generate_text,recheck_for_errors,get_recommended_links,get_interviewer_applicant_dialogue
+from .utilities import get_interviewer_applicant_dialogue,generate_lipsync_for_patch,generate_text,recheck_for_errors,get_recommended_links
 from concurrent.futures import ThreadPoolExecutor
 import os,json
 
 
 
-@api_view(['POST']) 
+@api_view(['POST'])
 def text_to_audio(request):
     try:
         text = request.data.get("text", "")
@@ -19,21 +19,15 @@ def text_to_audio(request):
             return Response({"error": "No text provided"}, status=400)
 
         # Generate dialogue if the input is a topic.
-        with ThreadPoolExecutor(max_workers=2) as executer:
-                dialogue = executer.submit(generate_text,text)
-                links =executer.submit(get_recommended_links, text)
-
-                result_text = dialogue.result()
-                recommended_links  =links.result()
-
-
-        # print(f"recommended links are: {recommended_links}")
+        # convo_client = resources.get_convo_client()
+        result_text = generate_text(text)
         print(f"Generated dialogue: {result_text}")
 
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         output_folder = os.path.join(BASE_DIR, "../backend/media")
         output_folder = os.path.abspath(output_folder)
         
+        #whisper_client = resources.get_whisper_client()
 
         # Process the pipeline for each conversation line concurrently.
         pipeline_results = process_conversation_pipeline(result_text, output_folder, max_workers=4)
@@ -52,7 +46,6 @@ def text_to_audio(request):
             "message": "Pipeline executed successfully",
             "pipeline_results": final_result,
             "metadata_path": file_name,
-            "recommendation_links":recommended_links
         }, status=201)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
@@ -166,4 +159,3 @@ def single_model(request):
         except Exception as e:
             print(f"Error in pipeline: {e}")
             return Response({"error": str(e)}, status=500)
-        
